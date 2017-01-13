0b1de0ead2145d42b5e8f06151a4086b85ee05ca
Tom Ross <Tom.Ross@redhat.com>
2017-01-13 10:06:45 +0000

    Fixed the dimond operator issue is 1.6 source code.

--- ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/jca/TransactionImporterImple.java
+++ ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/jca/TransactionImporterImple.java
@@ -236,7 +236,7 @@ public class TransactionImporterImple implements TransactionImporter
 		boolean isNew = false;
 		SubordinateXidImple importedXid = new SubordinateXidImple(mapKey);
 		// We need to store the imported transaction in a volatile field holder so that it can be shared between threads
-		AtomicReference<TransactionImple> holder = new AtomicReference<>();
+		AtomicReference<TransactionImple> holder = new AtomicReference<TransactionImple>();
 		AtomicReference<TransactionImple> existing;
 
 		if ((existing = _transactions.putIfAbsent(importedXid, holder)) != null) {
@@ -280,6 +280,7 @@ public class TransactionImporterImple implements TransactionImporter
 	}
 
 	private static ConcurrentHashMap<SubordinateXidImple, AtomicReference<TransactionImple>> _transactions =
-			new ConcurrentHashMap<>();
+			new ConcurrentHashMap<SubordinateXidImple, AtomicReference<TransactionImple>>();
+
 }
 
